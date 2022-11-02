2125e24c95f9618779bf1e239e6e3e4ed3d6f3bf
Nathan Nowack <thrast36@gmail.com>
2022-11-02 13:50:09 -0500

    add tests for client creation methods

---
+++ tests/test_credentials.py
@@ -0,0 +1,15 @@
+import pytest
+from kubernetes.client import AppsV1Api, BatchV1Api, CoreV1Api
+
+
+@pytest.mark.parametrize(
+    "resource_type_method,client_type",
+    [
+        ("get_app_client", AppsV1Api),
+        ("get_batch_client", BatchV1Api),
+        ("get_core_client", CoreV1Api),
+    ],
+)
+def test_client_return_type(kubernetes_credentials, resource_type_method, client_type):
+    resource_specific_client = getattr(kubernetes_credentials, resource_type_method)()
+    assert isinstance(resource_specific_client, client_type)
