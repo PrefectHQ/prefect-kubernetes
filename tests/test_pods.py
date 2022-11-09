fb00ef01abe9b42bf71480edc099e316dc7d950a
Nathan Nowack <thrast36@gmail.com>
2022-11-09 15:11:06 -0600

    add pod tests

--- tests/test_pods.py
+++ tests/test_pods.py
@@ -1,4 +1,5 @@
 import pytest
+from kubernetes.client.exceptions import ApiValueError
 
 from prefect_kubernetes.pods import (
     create_namespaced_pod,
@@ -11,5 +12,115 @@ from prefect_kubernetes.pods import (
 )
 
 
-async def test_create_namespaced_pod(_mock_api_core_client, kubernetes_credentials):
-    await create_namespaced_pod.fn()
+async def test_invalid_body_raises_error(kubernetes_credentials):
+    with pytest.raises(ApiValueError):
+        await create_namespaced_pod.fn(
+            body=None, kubernetes_credentials=kubernetes_credentials
+        )
+    with pytest.raises(ApiValueError):
+        await patch_namespaced_pod.fn(
+            body=None, pod_name="", kubernetes_credentials=kubernetes_credentials
+        )
+
+
+async def test_create_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await create_namespaced_pod.fn(
+        body={"test": "a"},
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+
+    assert _mock_api_core_client.create_namespaced_pod.call_args[1]["body"] == {
+        "test": "a"
+    }
+    assert _mock_api_core_client.create_namespaced_pod.call_args[1]["a"] == "test"
+
+
+async def test_delete_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await delete_namespaced_pod.fn(
+        pod_name="test_pod",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert (
+        _mock_api_core_client.delete_namespaced_pod.call_args[1]["namespace"]
+        == "default"
+    )
+    assert _mock_api_core_client.delete_namespaced_pod.call_args[1]["a"] == "test"
+
+
+async def test_list_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await list_namespaced_pod.fn(
+        namespace="ns",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert _mock_api_core_client.list_namespaced_pod.call_args[1]["namespace"] == "ns"
+    assert _mock_api_core_client.list_namespaced_pod.call_args[1]["a"] == "test"
+
+
+async def test_patch_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await patch_namespaced_pod.fn(
+        body={"test": "a"},
+        pod_name="test_pod",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["body"] == {
+        "test": "a"
+    }
+    assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["name"] == "test_pod"
+    assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["a"] == "test"
+
+
+async def test_read_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await read_namespaced_pod.fn(
+        pod_name="test_pod",
+        namespace="ns",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert _mock_api_core_client.read_namespaced_pod.call_args[1]["name"] == "test_pod"
+    assert _mock_api_core_client.read_namespaced_pod.call_args[1]["namespace"] == "ns"
+    assert _mock_api_core_client.read_namespaced_pod.call_args[1]["a"] == "test"
+
+
+async def test_read_namespaced_pod_logs(kubernetes_credentials, _mock_api_core_client):
+    await read_namespaced_pod_logs.fn(
+        pod_name="test_pod",
+        container="test_container",
+        namespace="ns",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert (
+        _mock_api_core_client.read_namespaced_pod_log.call_args[1]["name"] == "test_pod"
+    )
+    assert (
+        _mock_api_core_client.read_namespaced_pod_log.call_args[1]["namespace"] == "ns"
+    )
+    assert (
+        _mock_api_core_client.read_namespaced_pod_log.call_args[1]["container"]
+        == "test_container"
+    )
+    assert _mock_api_core_client.read_namespaced_pod_log.call_args[1]["a"] == "test"
+
+
+async def test_replace_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
+    await replace_namespaced_pod.fn(
+        pod_name="test_pod",
+        body={"test": "a"},
+        namespace="ns",
+        a="test",
+        kubernetes_credentials=kubernetes_credentials,
+    )
+    assert (
+        _mock_api_core_client.replace_namespaced_pod.call_args[1]["name"] == "test_pod"
+    )
+    assert (
+        _mock_api_core_client.replace_namespaced_pod.call_args[1]["namespace"] == "ns"
+    )
+    assert _mock_api_core_client.replace_namespaced_pod.call_args[1]["body"] == {
+        "test": "a"
+    }
+    assert _mock_api_core_client.replace_namespaced_pod.call_args[1]["a"] == "test"
