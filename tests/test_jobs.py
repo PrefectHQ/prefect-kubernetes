d859fc02463845e9925870f30dabc6c8923606c0
Nathan Nowack <thrast36@gmail.com>
2022-11-10 21:28:58 -0600

    add tests verifying model inputs

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,5 +1,6 @@
 import pytest
 from kubernetes.client.exceptions import ApiValueError
+from kubernetes.client.models import V1Job
 
 from prefect_kubernetes.jobs import (
     create_namespaced_job,
@@ -11,7 +12,7 @@ from prefect_kubernetes.jobs import (
 )
 
 
-async def test_invalid_body_raises_error(kubernetes_credentials):
+async def test_null_body_raises_error(kubernetes_credentials):
     with pytest.raises(ApiValueError):
         await create_namespaced_job.fn(
             body=None, kubernetes_credentials=kubernetes_credentials
@@ -20,29 +21,33 @@ async def test_invalid_body_raises_error(kubernetes_credentials):
         await patch_namespaced_job.fn(
             body=None, job_name="", kubernetes_credentials=kubernetes_credentials
         )
+    with pytest.raises(ApiValueError):
+        await replace_namespaced_job.fn(
+            body=None, job_name="", kubernetes_credentials=kubernetes_credentials
+        )
 
 
 async def test_create_namespaced_job(kubernetes_credentials, _mock_api_batch_client):
     await create_namespaced_job.fn(
-        body={"test": "a"},
+        body=V1Job(**{"metadata": {"name": "test-job"}}),
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
 
-    assert _mock_api_batch_client.create_namespaced_job.call_args[1]["body"] == {
-        "test": "a"
-    }
+    assert _mock_api_batch_client.create_namespaced_job.call_args[1][
+        "body"
+    ].metadata == {"name": "test-job"}
     assert _mock_api_batch_client.create_namespaced_job.call_args[1]["a"] == "test"
 
 
 async def test_delete_namespaced_job(kubernetes_credentials, _mock_api_batch_client):
     await delete_namespaced_job.fn(
-        job_name="test_job",
+        job_name="test-job",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
     assert (
-        _mock_api_batch_client.delete_namespaced_job.call_args[1]["name"] == "test_job"
+        _mock_api_batch_client.delete_namespaced_job.call_args[1]["name"] == "test-job"
     )
     assert _mock_api_batch_client.delete_namespaced_job.call_args[1]["a"] == "test"
 
@@ -59,47 +64,47 @@ async def test_list_namespaced_job(kubernetes_credentials, _mock_api_batch_clien
 
 async def test_patch_namespaced_job(kubernetes_credentials, _mock_api_batch_client):
     await patch_namespaced_job.fn(
-        body={"test": "a"},
-        job_name="test_job",
+        body=V1Job(**{"metadata": {"name": "test-job"}}),
+        job_name="test-job",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
-    assert _mock_api_batch_client.patch_namespaced_job.call_args[1]["body"] == {
-        "test": "a"
-    }
+    assert _mock_api_batch_client.patch_namespaced_job.call_args[1][
+        "body"
+    ].metadata == {"name": "test-job"}
     assert (
-        _mock_api_batch_client.patch_namespaced_job.call_args[1]["name"] == "test_job"
+        _mock_api_batch_client.patch_namespaced_job.call_args[1]["name"] == "test-job"
     )
     assert _mock_api_batch_client.patch_namespaced_job.call_args[1]["a"] == "test"
 
 
 async def test_read_namespaced_job(kubernetes_credentials, _mock_api_batch_client):
     await read_namespaced_job.fn(
-        job_name="test_job",
+        job_name="test-job",
         namespace="ns",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
-    assert _mock_api_batch_client.read_namespaced_job.call_args[1]["name"] == "test_job"
+    assert _mock_api_batch_client.read_namespaced_job.call_args[1]["name"] == "test-job"
     assert _mock_api_batch_client.read_namespaced_job.call_args[1]["namespace"] == "ns"
     assert _mock_api_batch_client.read_namespaced_job.call_args[1]["a"] == "test"
 
 
 async def test_replace_namespaced_job(kubernetes_credentials, _mock_api_batch_client):
     await replace_namespaced_job.fn(
-        job_name="test_job",
-        body={"test": "a"},
+        job_name="test-job",
+        body=V1Job(**{"metadata": {"name": "test-job"}}),
         namespace="ns",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
     assert (
-        _mock_api_batch_client.replace_namespaced_job.call_args[1]["name"] == "test_job"
+        _mock_api_batch_client.replace_namespaced_job.call_args[1]["name"] == "test-job"
     )
     assert (
         _mock_api_batch_client.replace_namespaced_job.call_args[1]["namespace"] == "ns"
     )
-    assert _mock_api_batch_client.replace_namespaced_job.call_args[1]["body"] == {
-        "test": "a"
-    }
+    assert _mock_api_batch_client.replace_namespaced_job.call_args[1][
+        "body"
+    ].metadata == {"name": "test-job"}
     assert _mock_api_batch_client.replace_namespaced_job.call_args[1]["a"] == "test"
--- tests/test_pods.py
+++ tests/test_pods.py
@@ -1,5 +1,6 @@
 import pytest
 from kubernetes.client.exceptions import ApiValueError
+from kubernetes.client.models import V1DeleteOptions, V1Pod
 
 from prefect_kubernetes.pods import (
     create_namespaced_pod,
@@ -25,28 +26,44 @@ async def test_invalid_body_raises_error(kubernetes_credentials):
 
 async def test_create_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
     await create_namespaced_pod.fn(
-        body={"test": "a"},
+        body=V1Pod(**{"metadata": {"name": "test-pod"}}),
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
 
-    assert _mock_api_core_client.create_namespaced_pod.call_args[1]["body"] == {
-        "test": "a"
-    }
+    assert _mock_api_core_client.create_namespaced_pod.call_args[1][
+        "body"
+    ].metadata == {"name": "test-pod"}
     assert _mock_api_core_client.create_namespaced_pod.call_args[1]["a"] == "test"
 
 
 async def test_delete_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
     await delete_namespaced_pod.fn(
-        pod_name="test_pod",
-        a="test",
         kubernetes_credentials=kubernetes_credentials,
+        pod_name="test_pod",
+        body=V1DeleteOptions(grace_period_seconds=42),
+        a="test",
     )
     assert (
         _mock_api_core_client.delete_namespaced_pod.call_args[1]["namespace"]
         == "default"
     )
     assert _mock_api_core_client.delete_namespaced_pod.call_args[1]["a"] == "test"
+    assert (
+        _mock_api_core_client.delete_namespaced_pod.call_args[1][
+            "body"
+        ].grace_period_seconds
+        == 42
+    )
+
+
+async def test_bad_v1_delete_options(kubernetes_credentials, _mock_api_core_client):
+    with pytest.raises(TypeError):
+        await delete_namespaced_pod.fn(
+            kubernetes_credentials=kubernetes_credentials,
+            pod_name="test_pod",
+            body=V1DeleteOptions(skrrrt_skrrrt="yeehaw"),
+        )
 
 
 async def test_list_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
@@ -61,13 +78,13 @@ async def test_list_namespaced_pod(kubernetes_credentials, _mock_api_core_client
 
 async def test_patch_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
     await patch_namespaced_pod.fn(
-        body={"test": "a"},
+        body=V1Pod(**{"metadata": {"name": "test-pod"}}),
         pod_name="test_pod",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
     )
-    assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["body"] == {
-        "test": "a"
+    assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["body"].metadata == {
+        "name": "test-pod"
     }
     assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["name"] == "test_pod"
     assert _mock_api_core_client.patch_namespaced_pod.call_args[1]["a"] == "test"
@@ -109,7 +126,7 @@ async def test_read_namespaced_pod_logs(kubernetes_credentials, _mock_api_core_c
 async def test_replace_namespaced_pod(kubernetes_credentials, _mock_api_core_client):
     await replace_namespaced_pod.fn(
         pod_name="test_pod",
-        body={"test": "a"},
+        body=V1Pod(**{"metadata": {"name": "test-pod"}}),
         namespace="ns",
         a="test",
         kubernetes_credentials=kubernetes_credentials,
@@ -120,7 +137,19 @@ async def test_replace_namespaced_pod(kubernetes_credentials, _mock_api_core_cli
     assert (
         _mock_api_core_client.replace_namespaced_pod.call_args[1]["namespace"] == "ns"
     )
-    assert _mock_api_core_client.replace_namespaced_pod.call_args[1]["body"] == {
-        "test": "a"
-    }
+    assert _mock_api_core_client.replace_namespaced_pod.call_args[1][
+        "body"
+    ].metadata == {"name": "test-pod"}
     assert _mock_api_core_client.replace_namespaced_pod.call_args[1]["a"] == "test"
+
+
+@pytest.mark.parametrize(
+    "task_accepting_pod",
+    [create_namespaced_pod, patch_namespaced_pod, replace_namespaced_pod],
+)
+async def test_bad_v1_pod_kwargs(kubernetes_credentials, task_accepting_pod):
+    with pytest.raises(TypeError):
+        await task_accepting_pod.fn(
+            body=V1Pod(**{"random_not_real": "shabba-ranks"}),
+            kubernetes_credentials=kubernetes_credentials,
+        )
