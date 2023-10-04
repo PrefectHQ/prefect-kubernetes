917f1b2679f23b55c9762dcd9a0cfb56b7d6dc71
Chris Guidry <chris.g@prefect.io>
2023-10-04 13:42:29 -0400

    Conditional imports to support pydantic>2

--- prefect_kubernetes/worker.py
+++ prefect_kubernetes/worker.py
@@ -113,7 +113,13 @@ from prefect.workers.base import (
     BaseWorker,
     BaseWorkerResult,
 )
-from pydantic import Field, validator
+from pydantic import VERSION as PYDANTIC_VERSION
+
+if PYDANTIC_VERSION.startswith("2."):
+    from pydantic.v1 import Field, validator
+else:
+    from pydantic import Field, validator
+
 from typing_extensions import Literal
 
 from prefect_kubernetes.events import KubernetesEventsReplicator
